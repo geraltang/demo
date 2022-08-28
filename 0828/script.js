ba68601ac0fb2dd468a2685ce3b2929b92f79744
geraltang <geraltangsmile@gmail.com>
2022-08-28 17:05:17 +0930

    08282

---
+++ 0828/script.js
@@ -0,0 +1,36 @@
+// function getTotalNum() {
+//     console.log('5');
+// }
+// getTotalNum()
+
+// function calculateSum(num1, num2) {
+//     console.log(num1 + num2)
+// }
+// calculateSum(1, 2)
+
+// let ABC = function (B) {
+//     console.log(B * 2);
+// }
+
+// let DEF = function (C) {
+//     console.log(C)
+//     ABC(C)
+// }
+
+// DEF(3)
+
+// const HIJ = (a, b) => {
+//     console.log(a + b);
+// }
+// HIJ(2, 3)
+
+class A {
+    constructor(name) {
+        this.name = name;
+    }
+    toString() {
+        console.log(`name:${this.name}`)
+    }
+}
+const B = new A('Peter')
+B.toString()
